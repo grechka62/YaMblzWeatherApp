4e007d25240b23c0f3b200212d3bfc3027f77e3f
Grechka62 <grechka62@yandex.ru>
2017-07-09 22:12:15 +0300

    Заглушка для экрана настроек

--- app/src/main/java/me/grechka/yamblz/yamblzweatherapp/SettingsFragment.java
+++ app/src/main/java/me/grechka/yamblz/yamblzweatherapp/SettingsFragment.java
@@ -2,7 +2,10 @@ package me.grechka.yamblz.yamblzweatherapp;
 
 
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,17 +16,16 @@ import android.view.ViewGroup;
  */
 public class SettingsFragment extends Fragment {
 
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.action_settings);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        return view;
     }
 
 }
